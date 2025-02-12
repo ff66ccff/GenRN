97cfcc3375111aa02c23494c8ce696a92b7d6d2c
ff66ccff <124170277+ff66ccff@users.noreply.github.com>
2025-02-12 17:48:32 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 1
-        versionName = "4.0"
+        versionName = "4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
